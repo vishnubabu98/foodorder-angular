5e92c621ca265781120cbeb60d59680c34b6b17f
vishnu <vishnu@NTP-LAP-767.chn.nestgroup.net>
2022-11-28 21:07:33 +0530

    add readvalues

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -7,4 +7,34 @@ import { Component } from '@angular/core';
 })
 export class RegisterComponent {
 
+  Name=""
+  Address=""
+  Pincode=""
+  Emailid=""
+  Phoneno=""
+  Username=""
+  Password=""
+  Confirmpassword=""
+
+  readValues=()=>
+  {
+    let data:any=
+    {
+        "Name":this.Name,
+        "Address":this.Address,
+        "Pincode":this.Pincode,
+        "Emailid":this.Emailid,
+        "Phoneno":this.Phoneno,
+        "Username":this.Username,
+        "Password":this.Password,
+        "ConfirmPassword":this.Confirmpassword
+    }
+    console.log(data)
+  }
+
+
+
+
+
+
 }
